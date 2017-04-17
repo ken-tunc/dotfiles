969879adcc3aeea59ae7bcafe6d766f46f9fe2ab
ken_tunc <tunc.adfuisses.53@gmail.com>
2017-04-17 11:15:06 +0900

    Add vscode settings.

--- setup.sh
+++ setup.sh
@@ -15,6 +15,8 @@ main() {
   install_symlink ".vim"
   install_symlink ".vimrc"
   install_symlink ".zshrc"
+  install_symlink "Library/Application Support/Code/User/locale.json"
+  install_symlink "Library/Application Support/Code/User/settings.json"
 
   setup_vim
 }
