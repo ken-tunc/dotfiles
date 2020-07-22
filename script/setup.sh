59266a4dfadd39af00f1475c7e6f6c716362dacb
ken_tunc <tunc.adfuisses.53@gmail.com>
2020-07-23 00:53:33 +0900

    setup: Refactor.

--- script/setup.sh
+++ script/setup.sh
@@ -2,13 +2,33 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)"
 DOTFILES_DIR="$(dirname "$SCRIPT_DIR")"
-NO_DEPS=
 
 main() {
-  cd "$DOTFILES_DIR"
+  local no_deps=0
 
+  for opt in "$@"; do
+    case "$opt" in
+      -h|--help)
+        print_usage
+        ;;
+      --no-deps)
+        no_deps=1
+        shift 1
+        ;;
+      *) ;;
+    esac
+  done
+
+  cd "$DOTFILES_DIR"
   git submodule update --init --remote
 
+  [[ "$no_deps" = 1 ]] || install_deps
+
+  setup_main
+  setup_gpg
+}
+
+setup_main() {
   install_symlink ".clang-format"
   install_symlink ".config/fish/config.fish"
   install_symlink ".config/fish/functions/fish_user_key_bindings.fish"
@@ -23,10 +43,9 @@ main() {
   install_symlink ".vim"
   install_symlink ".zshenv"
   install_symlink ".zshrc"
+}
 
-  [[ -z "$NO_DEPS" ]] && install_deps
-
-  # Setup GnuPG
+setup_gpg() {
   if [[ ! -d ~/.gnupg ]]; then
     mkdir ~/.gnupg
   fi
@@ -40,6 +59,11 @@ main() {
   install_symlink ".gnupg/gpg-agent.conf"
 }
 
+install_deps() {
+  echo "Install dependencies..."
+  brew update && brew bundle --file "$SCRIPT_DIR/Brewfile" --no-lock
+}
+
 install_symlink() {
   local dir="$(dirname "$HOME/$1")"
   [[ ! -d "$dir" ]] && mkdir -p "$dir"
@@ -47,29 +71,16 @@ install_symlink() {
   ln -snf "$DOTFILES_DIR/home/$1" "$HOME/$1"
 }
 
-install_deps() {
-  echo "Install dependencies..."
-  brew update && brew bundle --file "$SCRIPT_DIR/Brewfile" --no-lock
-}
-
 print_usage() {
   cat << EOF
-  usage: $0 [options]
+usage: $0 [options]
 
-  options:
-    -h, --help  Print help messages.
-    --no-deps   Setup without installing dependencies.
+options:
+  -h, --help  Print help messages.
+  --no-deps   Setup without installing dependencies.
 EOF
 
   exit
 }
 
-for opt in "$@"; do
-  case "$opt" in
-    -h|--help) print_usage ;;
-    --no-deps) NO_DEPS=1 ;;
-    *) ;;
-  esac
-done
-
-main
+main "$@"
